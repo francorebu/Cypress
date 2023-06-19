35e2e5d4e1d7e2ae9e27277b3d3ad737b314dfaa
Franco R <francoarebuffi@gmail.com>
2023-06-18 23:41:17 -0300

    second commit

--- cypress/e2e/uitests/loginUnSuccesfulTest.cy.js
+++ cypress/e2e/uitests/loginUnSuccesfulTest.cy.js
@@ -4,13 +4,12 @@ import { TopBarPage } from "../pages/topbar_page";
 const loginPage = new LoginPage();
 const topBarPage = new TopBarPage();
 
-describe('Login invalid Spec', () => {
-  it('Login Unsuccesful', () => {
+it('Login Unsuccesful', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
     loginPage.login("Admin", "invalidpass");
 
     loginPage.isErrorMsgVisible().then((isVisible) => {
       expect(isVisible).to.be.true; // Assertion to validate the returned value
     });
-  });
 });
+
